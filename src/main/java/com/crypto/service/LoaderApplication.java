24cf6e547b25eb3d98f293655cc255e904f18d0c
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-03-13 16:52:28 +0300

    not refactored implemenation of TicketsDataReader

--- src/main/java/com/crypto/service/LoaderApplication.java
+++ src/main/java/com/crypto/service/LoaderApplication.java
@@ -1,30 +1,12 @@
 package com.crypto.service;
 
-import com.clickhouse.jdbc.ClickHouseConnection;
-import com.crypto.service.dao.ClickHouseDAO;
-import com.crypto.service.util.ConnectionHandler;
 import com.crypto.service.util.TicketsDataReader;
-
-import java.sql.SQLException;
-import java.util.List;
 
 public class LoaderApplication {
 
   public static void main(String[] args) {
 
-    try (ClickHouseConnection connection = ConnectionHandler.initJDBCConnection()) {
-      // For JDBC Connection
-      ClickHouseDAO clickHouseDAO = new ClickHouseDAO(connection);
-      clickHouseDAO.countRecords();
-
-
-      // For JavaClient Connection
-      // ClickHouseNode server = ConnectionHandler.initJavaClientConnection();
-      // ClickHouseDAO clickHouseDAO = new ClickHouseDAO(server);
-      // clickHouseDAO.insertFromFile();
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+    TicketsDataReader ticketsDataReader = new TicketsDataReader();
+    ticketsDataReader.readExecutor();
   }
 }
--- src/main/java/com/crypto/service/util/TicketsDataReader.java
+++ src/main/java/com/crypto/service/util/TicketsDataReader.java
@@ -1,28 +1,91 @@
 package com.crypto.service.util;
 
+import com.clickhouse.jdbc.ClickHouseConnection;
+import com.crypto.service.dao.ClickHouseDAO;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import org.checkerframework.checker.units.qual.C;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TicketsDataReader {
-  private static String getCurrentDate() {
+  private ClickHouseDAO clickHouseDAO;
+
+  private final int PARTS_QUANTITY = 32;
+  private final int THREADS_COUNT = PARTS_QUANTITY;
+  private final String SOURCE_PATH;
+
+  public TicketsDataReader() {
+    String currentDate = getCurrentDate();
+    SOURCE_PATH = PropertiesLoader.loadJDBCProp().getProperty("DATA_PATH") + "/" + currentDate;
+  }
+
+  private String getCurrentDate() {
     LocalDate currentDate = LocalDate.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     return currentDate.format(formatter);
   }
 
-  private static List<String> getFilesInDirectory() {
-    String currentDate = getCurrentDate();
-    String sourcePath =
-        PropertiesLoader.loadJDBCProp().getProperty("DATA_PATH") + "/" + currentDate;
-
-    File searchDirectory = new File(sourcePath);
+  private List<String> getFilesInDirectory() {
+    File searchDirectory = new File(SOURCE_PATH);
     return ImmutableList.copyOf(Objects.requireNonNull(searchDirectory.list()));
+  }
+
+  public void readExecutor() {
+    List<String> ticketNames = getFilesInDirectory();
+    System.out.println(ticketNames.size());
+
+    List<List<String>> ticketParts = Lists.partition(ticketNames, ticketNames.size()/PARTS_QUANTITY);
+    System.out.println(ticketParts.size());
+
+    try (ClickHouseConnection connection = ConnectionHandler.initJDBCConnection();
+        ExecutorService service = Executors.newFixedThreadPool(THREADS_COUNT)) {
+
+      this.clickHouseDAO = new ClickHouseDAO(connection);
+
+      for (List<String> ticketPartition : ticketParts) {
+        service.execute(new FileProcessor(ticketPartition));
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  class FileProcessor implements Runnable {
+    private final List<String> partition;
+
+    public FileProcessor(List<String> partition) {
+      this.partition = partition;
+    }
+
+    @Override
+    public void run() {
+      for (String fileName : partition) {
+        collectData(fileName);
+      }
+    }
+
+    private void collectData(String fileName) {
+      try {
+        List<String> ticketInfo = Files.readAllLines(Paths.get(SOURCE_PATH + "/" + fileName));
+        clickHouseDAO.insertData(ticketInfo);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
